fc28253e284cf8d321b11ea07fafbea1d7215b47
Chayma <124793597+HeChayma@users.noreply.github.com>
2025-10-24 17:38:18 +0200

    start

---
+++ src/test/java/com/example/tp1_kafka/Tp1KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp1_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
